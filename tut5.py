64fe60f0a7ad43a1272115d5331554478af050a7
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-09 20:56:39 +0530

    added

--- tut5.py
+++ tut5.py
@@ -1,2 +1,14 @@
 # Shape and Reshape in NumPy
 
+import numpy as np
+ 
+var=np.array([[1,2],[3,4]])
+print(var)
+print()
+print(var.shape)  # It gives the matrix size
+print()
+
+
+var2=np.array([1,2,3,4,5,6,7,8,9])
+x=var2.reshape(3,3)  # It makes 1D array into multidimensional array (rows,columns)
+print(x)
