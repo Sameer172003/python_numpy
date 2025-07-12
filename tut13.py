c0e428e4b11f51ed39910a93f154e558c507b3d1
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-12 13:26:47 +0530

    added

---
+++ tut13.py
@@ -0,0 +1,27 @@
+# Shuffle, Unique, Resize, Flatten, Ravel Functions in numpy arrays
+
+import numpy as np
+
+# Shuffle Function
+
+var1=np.array([1,2,3,4,5])
+np.random.shuffle(var1)
+print(var1)
+print()
+
+# Unique Function
+
+var2=np.array([1,2,2,1,3,4,4,5])
+x=np.unique(var2)
+print(x)
+print()
+
+# Resize function
+
+var3=np.array([1,2,3,4,5,6])
+y=np.resize(var3,(2,3))
+print(y)
+print()
+
+# Flatten function
+
