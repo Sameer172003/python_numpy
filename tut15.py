c69fe00f77ac6c8ee4e245fb86bc54007ed32a08
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-12 17:22:09 +0530

    added

---
+++ tut15.py
@@ -0,0 +1,22 @@
+# NumPy - Matrix
+
+import numpy as np
+
+v1=np.matrix([[1,2],[4,5]])
+v2=np.matrix([[7,8],[10,11]])
+
+res1=v1+v2
+print(res1)
+print()
+
+res2=v1-v2
+print(res2)
+print()
+
+res3=v1.dot(v2)
+print(res3)
+print()
+
+res4=v1/v2
+print(res4)
+print()
